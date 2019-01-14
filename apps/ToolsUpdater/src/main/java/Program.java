414ea2ac0c2869829af1d1e9eda19cc4aee34869
Thad House <thadhouse1@gmail.com>
2019-01-13 16:56:15 -0800

    Fix tool copies to overwrite files
    I blame java (and me for not testing...)

--- apps/ToolsUpdater/src/main/java/Program.java
+++ apps/ToolsUpdater/src/main/java/Program.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
 
 import com.google.gson.Gson;
@@ -36,10 +37,12 @@ public class Program {
         Path artifactPath = Paths.get(mavenDir, artifact.groupId.replace('.', File.separatorChar), artifact.artifactId, artifact.version, artifactFileName);
         if (artifactPath.toFile().exists()) {
           try {
-            Files.copy(artifactPath, Paths.get(toolsPath, tool.name + ".jar"));
-            Files.copy(Paths.get(toolsPath, "ScriptBase.vbs"), Paths.get(toolsPath, tool.name + ".vbs"));
-            Files.copy(Paths.get(toolsPath, "ScriptBase.py"), Paths.get(toolsPath, tool.name + ".py"));
+            Files.copy(artifactPath, Paths.get(toolsPath, tool.name + ".jar"), StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(Paths.get(toolsPath, "ScriptBase.vbs"), Paths.get(toolsPath, tool.name + ".vbs"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
+            Files.copy(Paths.get(toolsPath, "ScriptBase.py"), Paths.get(toolsPath, tool.name + ".py"), StandardCopyOption.REPLACE_EXISTING, StandardCopyOption.COPY_ATTRIBUTES);
           } catch (IOException e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
           }
         }
       });
